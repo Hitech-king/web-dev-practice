50423fbbb4cd4330a8abb131af4431cfe27eebbc
Hitech-King <hitechbhusara@gmail.com>
2025-07-25 13:21:08 +0530

    larned about dates and time in JS

---
+++ 03_javaScript/01_basics/06_dates.js
@@ -0,0 +1,13 @@
+let myDate = new Date();
+
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+console.log(myDate.toTimeString());
+
+let myCreatedDate = new Date(2025, 6, 25,13,7); // year, month(start with 0) , date , hour , min ...
+console.log(myDate.toLocaleString());
+
